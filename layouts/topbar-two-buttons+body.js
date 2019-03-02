3fb412f9c319e8921881d61e42d891cd552e6b47
Alex <370500+alexyk@users.noreply.github.com>
2019-03-02 17:35:46 +0200

    Retab code indentation

--- layouts/topbar-two-buttons+body.js
+++ layouts/topbar-two-buttons+body.js
@@ -34,7 +34,7 @@ export default class TopBarTwoButtonsBody extends Component {
             <Text style={{color:'white',backgroundColor:'green'}}>Right</Text>
           </View>
       )
-  }
+    }
   }
   
   
